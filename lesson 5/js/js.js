9addde6c179c84ec897fe1717b28178dcaa4eb7e
rufuuso1 <ort20011@byui.edu>
2021-02-06 11:21:11 -0300

    lesson 5

---
+++ lesson 5/js/js.js
@@ -0,0 +1,40 @@
+const menuBtn = document.querySelector(".menu-btn");
+const hamburguer = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+const today= new Date();
+const todayNum= today.getDate();
+const offer= document.getElementById("saturday")
+let showMenu = false;
+
+if (todayNum == 5) {
+    offer.classList.add("showme")
+}else{ offer.classList.add("hideme")}
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburguer.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        navItems.forEach(item => item.classList.add("open"))
+
+
+        showMenu= true;
+    }else{
+
+        hamburguer.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        navItems.forEach(item => item.classList.remove("open"))
+        showMenu = false;
+    }
+
+}
+
+
+
+var lastModified = document.lastModified;
+document.getElementById("updated").innerHTML = "Last Updated : " + lastModified;
